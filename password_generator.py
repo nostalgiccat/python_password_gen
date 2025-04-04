c6ceb57b5fc14c13c147d5d303bef7e5ac09a997
nostalgiccat <nesdom27@gmail.com>
2025-04-03 21:34:36 -0700

    initial commit

---
+++ password_generator.py
@@ -0,0 +1,10 @@
+def generate_password():
+    pass
+
+def main():
+    print("Welcome to the Password Generator!")
+    # Ask user for options here
+    # Call generate_password()
+
+if __name__ == "__main__":
+    main()
